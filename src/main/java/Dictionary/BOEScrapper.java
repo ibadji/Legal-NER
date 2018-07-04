af62835e727636cd430e4dfc62bf1779d35079e4
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2018-07-04 18:22:00 +0200

    scrapper improved

--- src/main/java/Dictionary/BOEScrapper.java
+++ src/main/java/Dictionary/BOEScrapper.java
@@ -1,10 +1,12 @@
 package Dictionary;
 
 import Model.Law;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,11 +15,57 @@ import org.jsoup.select.Elements;
 /**
  * Scrapper of the BOE to get all the legislation.
  * https://www.boe.es/legislacion/legislacion.php?campo%5B0%5D=ID_SRC&dato%5B0%5D=1&operador%5B0%5D=and&campo%5B1%5D=NOVIGENTE&dato%5B1%5D=N&operador%5B1%5D=and&campo%5B2%5D=&dato%5B2%5D=Ley&accion=Buscar&checkbox_solo_tit=S&operador%5B2%5D=and&page_hits=999&sort_field%5B0%5D=PESO&sort_order%5B0%5D=desc&sort_field%5B1%5D=ref&sort_order%5B1%5D=asc
+ * Aviso legal del BOE: https://www.boe.es/sede_electronica/informacion/aviso_legal.php
  * @author vroddon
  */
 public class BOEScrapper {
     
+    
+    /**
+     * 
+     */
         public static void main(String[] args) throws IOException {
+        }
+
+/**
+ * Downloads the legislation from the BOE.
+ * Gets the official xml page from the boe uri making this transformation
+ * //                    https://www.boe.es/buscar/doc.php?id=BOE-A-1963-13975                    
+//                    https://www.boe.es/diario_boe/xml.php?id=BOE-A-1963-13975
+ */        
+        public static void downloadXML()
+        {
+            Set<Law> laws = Law.readLawsFile("./resources/other Codes used/boe_out.txt");
+            for(Law law : laws)
+            {
+                if (law==null)
+                    continue;
+                if (law.title.startsWith("Ley"))
+                {
+                    try{
+                    String urlxml = law.boe.replace("https://www.boe.es/buscar/doc.php?id=", "https://www.boe.es/diario_boe/xml.php?id=");
+                    String xml = LynxUtils.getHtmlContent(urlxml);
+                    String id = urlxml.replace("https://www.boe.es/diario_boe/xml.php?id=","");
+                    try{
+                        FileUtils.writeStringToFile(new File("./resources/spain/"+id+".xml"), xml, "UTF-8");
+                    }catch(Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                    }catch(Exception e2)
+                    {
+                        System.err.println("Error con " + law.boe);
+                        e2.printStackTrace();
+                    }
+                    
+                   // break;
+                }
+            }
+                
+        }
+        
+        public static void listDocuments()
+        {
             int num=999;
             Set<Law> leyes = new HashSet();
             String query1 ="https://www.boe.es/legislacion/legislacion.php?campo%5B0%5D=ID_SRC&dato%5B0%5D=1&operador%5B0%5D=and&campo%5B1%5D=NOVIGENTE&dato%5B1%5D=N&operador%5B1%5D=and&campo%5B2%5D=&dato%5B2%5D=Ley&accion=Buscar&checkbox_solo_tit=S&operador%5B2%5D=and&page_hits="+num+"&sort_field%5B0%5D=PESO&sort_order%5B0%5D=desc&sort_field%5B1%5D=ref&sort_order%5B1%5D=asc";
@@ -46,6 +94,7 @@ public class BOEScrapper {
 //                String attr2 = test.attr("abs:href");
                 leyes.add(ley);
             }
+            try{
             FileWriter fw2 = new FileWriter("resources\\other Codes used\\boe_out9.csv");
             FileWriter fw = new FileWriter("resources\\other Codes used\\boe_out9.txt");
             for(Law ley : leyes)
@@ -58,5 +107,9 @@ public class BOEScrapper {
             }
             fw.close();
             fw2.close();
+            }catch(Exception e)
+            {
+                
+            }
         }
 }
--- src/main/java/Model/Law.java
+++ src/main/java/Model/Law.java
@@ -1,6 +1,9 @@
 package Model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,11 +34,36 @@ public class Law {
     public String boe = "";
     public String resumen = "";
 
+    String id="";
+    
     //Other names and abbreviations
     public Set<String> nicknames = new HashSet();
 
-    
-    
+    public static Set<Law> readLawsFile(String filename) {
+        Set<Law> laws = new HashSet();
+        try {
+            FileInputStream fstream = new FileInputStream(filename);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+            String line="";
+
+            while ((line = br.readLine()) != null) {
+                try {
+//                    System.out.println(line);
+                    Law law = jsonToLaw(line);
+                    laws.add(law);
+                } catch (Exception e) {
+
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+
+        }
+
+        return laws;
+    }
+
     public static Law jsonToLaw(String json) {
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -46,13 +74,12 @@ public class Law {
         }
     }
 
-    public String toCSV()
-    {
-        String s ="";
-        s+= title +"\t" + this.boe +"\t" + this.legislator +"\t" + boe_consolidated ;
+    public String toCSV() {
+        String s = "";
+        s += title + "\t" + this.boe + "\t" + this.legislator + "\t" + boe_consolidated;
         return s;
     }
-    
+
     public String toString() {
         String s = "";
         ObjectMapper mapper = new ObjectMapper();
